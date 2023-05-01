23c1c85d3af127e7f94e1959c4513b0f16b589d8
Randi Crews <randireneecrews@gmail.com>
2023-05-01 00:34:57 -0400

    first

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://thisisanemailaddressbeepboop:leG5hDmPkMxKEL2M@cluster0.gku6pzb.mongodb.net/DemoDay?retryWrites=true&w=majority', 
+    'dbName': 'demoday'
+};
